0d85544e15adc2d51a21b7f6c915467c9c03ee3d
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-03-03 15:38:48 +0000

    finish implementing rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -59,8 +59,8 @@ provider.add_span_processor(processor)
 
 
 # Show this in the logs within the backend-flask app (STDOUT)
-simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(simple_processor)
+#simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+#provider.add_span_processor(simple_processor)
 
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
