298c458a2de0f886b5b1ac68ed7dcaa8c70b8193
Firdous2307 <124298708+Firdous2307@users.noreply.github.com>
2023-03-22 01:13:12 +0000

    updating changes

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/bash
 
 # npm install frontend
-cd/workspaces/aws-bootcamp-cruddur-2023/frontend-react-js && npm update -g && npm i;
+cd /workspaces/aws-bootcamp-cruddur-2023/frontend-react-js && npm update -g && npm i;
 
 # backend pip requirements
-cd/workspaces/aws-bootcamp-cruddur-2023/backend-flask && pip3 install -r requirements.txt;
+cd /workspaces/aws-bootcamp-cruddur-2023/backend-flask && pip3 install -r requirements.txt;
 
 #Postgresql
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list';
