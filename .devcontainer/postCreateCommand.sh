1a123b732a9a44c4769c652b6294cff69c8dfd5a
Firdous2307 <124298708+Firdous2307@users.noreply.github.com>
2023-03-22 01:59:49 +0000

    revert changes

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,7 +7,7 @@ cd /workspaces/aws-bootcamp-cruddur-2023/frontend-react-js && npm update -g && n
 cd /workspaces/aws-bootcamp-cruddur-2023/backend-flask && pip3 install -r requirements.txt;
 
 #Postgresql
-       curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
-      echo "deb http://apt.postgresql.org/pub/repo s/apt/ lsb_release -cs-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
-      sudo apt update
-      sudo apt install -y postgresql-client-13 libpq-de    
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list';
+wget --quiet -0 - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -;
+sudo apt-get update -y;
+sudo apt install -y postgresql-client-13 libpq-dev 
