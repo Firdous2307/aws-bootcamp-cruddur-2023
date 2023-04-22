24ae6beaab70ca6ac689bc612e11140478bfb455
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-04-22 13:23:06 +0000

    spaces

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,9 +2,9 @@
 INSERT INTO public.users (display_name, email,handle, cognito_user_id)
 VALUES
   ('Mohammed Firdous','mohammedfirdousaraoye@gmail.com' , 'Fird2307' ,'d7fef980-4fa9-459f-aec4-f067d5e56121');
-  ('Andrew Bayko', 'bayko@exampro.co' ,'bayko', 'MOCK'); 
-  ('Andrew Brown', 'andrew@exampro.co' , 'andrewbrown' , 'MOCK');
-  ('Londo Mollari', 'lmollari@centari.com','londo','MOCK');
+  ('Andrew Bayko','bayko@exampro.co' , 'bayko','MOCK'); 
+  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK');
+  ('Londo Mollari','lmollari@centari.com' , 'londo','MOCK');
 
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
