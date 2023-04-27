0e255aac2654d78578dca705ca1ed6d93e020abf
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-04-27 02:02:44 +0000

    having issues with gitpod

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,7 +1,7 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Mohammed Firdous','mohammedfirdousaraoye@gmail.com' , 'Fird2307' ,'d7fef980-4fa9-459f-aec4-f067d5e56121'),
+  ('Mohammed Firdous','mohammedfirdousaraoye@gmail.com' , 'Fird2307' ,'MOCK'),
   ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
   ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
