5db357c5fde6f790c6eba734ceb73dc21bfc552d
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-06-29 02:50:30 +0000

    kill all connections

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,10 +1,9 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Mohammed Firdous','mohammedfirdousaraoye+andrewbrown@gmail.com' , 'Firdous' ,'MOCK'),
-  ('Andrew Bayko','mohammedfirdousaraoye+bayko@gmail.com' , 'bayko' ,'MOCK');
+  ('Mohammed Firdous','mohammedfirdousaraoye@gmail.com' , 'Firdous' ,'MOCK'),
+  ('Andrew Bayko','bayko@cloudprojectbootcamp.com' , 'bayko' ,'MOCK'),
   ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK'); 
-
 
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
