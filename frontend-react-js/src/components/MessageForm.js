44b3c84ab5bd026df061ae844de76f11dcbe5c6d
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-07-03 02:14:59 +0000

    fixed expiring tokens

--- frontend-react-js/src/components/MessageForm.js
+++ frontend-react-js/src/components/MessageForm.js
@@ -3,6 +3,8 @@ import React from "react";
 import process from 'process';
 import { json, useParams } from 'react-router-dom';
 
+import {getAccessToken} from '../lib/CheckAuth';
+
 export default function ActivityForm(props) {
   const [count, setCount] = React.useState(0);
   const [message, setMessage] = React.useState('');
@@ -19,10 +21,18 @@ export default function ActivityForm(props) {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages`
       console.log('onsubmit payload', message)
+      let json = { 'message': message }
+      if (params.handle) {
+        json.handle = params.handle
+      } else {
+        json.message_group_uuid = params.message_group_uuid
+      }
+      await getAccessToken()
+      const access_token = localStorage.getItem("access_token")
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
-          'Authorization': `Bearer ${localStorage.getItem("access_token")}`,
+          'Authorization': `Bearer ${access_token}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
