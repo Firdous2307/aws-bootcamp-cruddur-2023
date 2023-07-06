7e8d1607ad118f047a829ed9cbace8ebcca63062
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-07-06 00:49:14 +0000

    Implemented Users Profile Page

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,40 +1,28 @@
-from datetime import datetime, timedelta, timezone
-from aws_xray_sdk.core import xray_recorder
+from lib.db import db
+#from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
-    try:
-      model = {
+     #try:
+    model = {
       'errors': None,
       'data': None
     }
-
-      now = datetime.now(timezone.utc).astimezone()
-    
-      if user_handle == None or len(user_handle) < 1:
-        model['errors'] = ['blank_user_handle']
-      else:
-        now = datetime.now()
-      results = [{
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
-        'created_at': (now - timedelta(days=1)).isoformat(),
-        'expires_at': (now + timedelta(days=31)).isoformat()
-      }]
+    if user_handle == None or len(user_handle) < 1:
+      model['errors'] = ['blank_user_handle']
+    else:
+      print("else:")
+      sql = db.template('users','show')
+      results = db.query_object_json(sql,{'handle': user_handle})
       model['data'] = results
-
        # X-ray
       # Start a subsegment
       #subsegment = xray_recorder.begin_segment('mock-data')
-
       #dict = {
       #  "now": now.isoformat(),
       #  "results-size": len(model['data'])
       #}
-
       #subsegment.put_metadata('key', dict, 'namespace')
-    finally:
+    #finally:
       # Close subsegment
       #xray_recorder.end_subsegment()
-
-      return model
+    return model
