9628fe7cccf9f0b3fb5933b415012c7bf5267cff
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-07-10 00:46:24 +0000

    added migrate comand in the setup

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -11,10 +11,13 @@ def handler(event:, context:)
    url = obj.presigned_url(:put, expires_in: 60 * 5 )
    url # this is the data that will be returned
    body = {url: url}.to_json 
-   { statusCode: 200, body: body }
+   { 
+    headers: {
+      "Access-Control-Allow-Headers": "*, Authorization",
+      "Access-Control-Allow-Origin": "https://3000-firdous2307-awsbootcamp-0tppvyfx7zi.ws-eu101.gitpod.io/",
+      "Access-Control-Allow-Methods": "OPTIONS, GET, POST"
+    },
+    statusCode: 200, 
+    body: body
+  }
 end
-
-puts handler(
-   event: {},
-   context: {}
-)
--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -22,6 +22,7 @@ export default function ProfileForm(props) {
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
+          'Origin': "https://3000-firdous2307-awsbootcamp-0tppvyfx7zi.ws-eu101.gitpod.io",
           'Authorization': `Bearer ${access_token}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
@@ -29,6 +30,7 @@ export default function ProfileForm(props) {
       let data = await res.json();
       if (res.status === 200) {
         console.log('presigned url',data)
+        return data.url
       } else {
         console.log(res)
       }
@@ -46,10 +48,12 @@ export default function ProfileForm(props) {
     const preview_image_url = URL.createObjectURL(file)
     console.log(filename,size,type)
 
+  const presignedurl = await s3uploadkey()
+    console.log('pp',presignedurl)
     try {
       console.log('s3upload')
       const backend_url = ""
-      const res = await fetch(backend_url, {
+      const res = await fetch(presignedurl, {
         method: "PUT",
         body: file,
         headers: {
@@ -57,7 +61,7 @@ export default function ProfileForm(props) {
       }})
       let data = await res.json();
       if (res.status === 200) {
-        console.log('presigned url',data)
+        setPresignedurl(data.url)
       } else {
         console.log(res)
       }
@@ -126,9 +130,6 @@ export default function ProfileForm(props) {
           </div>
           <div className="popup_content">
             
-          <div className="upload" onClick={s3uploadkey}>
-              Upload Avatar
-            </div>
           <input type="file" name="avatarupload" onChange={s3upload} />
             <div className="field display_name">
               <label>Display Name</label>
