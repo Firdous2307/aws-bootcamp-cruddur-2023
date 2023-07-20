3535958e2c2695a62b23c5a5318a8b6bedde6995
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-07-20 00:15:53 +0000

    debugging III

--- backend-flask/db/sql/users/show.sql
+++ backend-flask/db/sql/users/show.sql
@@ -1,32 +1,31 @@
-SELECT 
-  (SELECT COALESCE(row_to_json(object_row),'{}'::json) FROM (
-    SELECT
-      users.uuid,
-      users.handle,
-      users.display_name,
-      users.bio,
-      (
-       SELECT 
-        count(true) 
-       FROM public.activities
-       WHERE
-        activities.user_uuid = users.uuid
-       ) as cruds_count
-  ) object_row) as profile,
+SELECT
+  activities.uuid,
+  users.display_name,
+  users.handle,
+  activities.message,
+  activities.replies_count,
+  activities.reposts_count,
+  activities.likes_count,
+  activities.expires_at,
+  activities.created_at,
   (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
-    SELECT
-      activities.uuid,
-      users.display_name,
-      users.handle,
-      activities.message,
-      activities.created_at,
-      activities.expires_at
-    FROM public.activities
-    WHERE
-      activities.user_uuid = users.uuid
-    ORDER BY activities.created_at DESC 
-    LIMIT 40
-  ) array_row) as activities
-FROM public.users
-WHERE
-  users.handle = %(handle)s
+  SELECT
+    replies.uuid,
+    reply_users.display_name,
+    reply_users.handle,
+    replies.message,
+    replies.replies_count,
+    replies.reposts_count,
+    replies.likes_count,
+    replies.reply_to_activity_uuid,
+    replies.created_at
+  FROM public.activities replies
+  LEFT JOIN public.users reply_users ON reply_users.uuid = replies.user_uuid
+  WHERE
+    replies.reply_to_activity_uuid = activities.uuid
+  ORDER BY  activities.created_at ASC
+  ) array_row) as replies
+FROM public.activities
+LEFT JOIN public.users ON users.uuid = activities.user_uuid
+WHERE activities.uuid = %(uuid)s
+ORDER BY activities.created_at DESC
