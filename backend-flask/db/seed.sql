9ab14861733fd68aa1aefaab8f8405a2915fd598
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-07-25 03:04:03 +0000

    changes

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -12,7 +12,7 @@ VALUES
   (
     (SELECT uuid from public.users WHERE users.handle = 'Firdous' LIMIT 1),
     'This was imported as seed data!',
-        current_timestamp + interval '10 day'
+     current_timestamp + interval '10 day'
   ),
   (
     (SELECT uuid from public.users WHERE users.handle = 'altbrown' LIMIT 1),
